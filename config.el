5406fd90598ac648ea53420fdc632741f9947e90
Nikolai Weidt <aramus92@gmail.com>
2020-06-06 13:16:06 +0200

    initial commit

---
+++ config.el
@@ -0,0 +1,89 @@
+(setq user-full-name "Nikolai Weidt")
+(setq user-mail-address "weidtn@gmail.com")
+
+(setq doom-font (font-spec :family "hack" :size 14))
+
+(setq +pretty-code-enabled-modes nil)
+
+(setq display-line-numbers-type 'relative)
+
+(use-package! zenburn-theme
+  :ensure t
+  :config
+  (load-theme 'zenburn t))
+
+(setq calendar-week-start-day 1
+          calendar-day-name-array ["Sonntag" "Montag" "Dienstag" "Mittwoch"
+                                   "Donnerstag" "Freitag" "Samstag"]
+          calendar-month-name-array ["Januar" "Februar" "März" "April" "Mai"
+                                     "Juni" "Juli" "August" "September"
+                                     "Oktober" "November" "Dezember"])
+
+(setq org-bullets-bullet-list '("✖" "✚")
+      org-ellipsis "▼")
+(setq org-babel-python-command "python3")
+
+(with-eval-after-load "ox-latex"
+  (add-to-list 'org-latex-classes
+               '("koma-article" "\\documentclass{scrartcl}"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+
+(use-package! org-ref
+  :after org
+;;  :init
+;;  :config
+)
+
+(use-package! platformio-mode
+:config (add-to-list 'company-backends 'company-irony)
+)
+
+(use-package! irony
+:config
+(add-to-list 'irony-supported-major-modes 'arduino-mode)
+(add-to-list 'irony-lang-compile-option-alist '(arduino-mode . "c++")))
+
+(add-hook! arduino-mode #'irony-mode 'irony-eldoc 'platformio-conditionally-enable)
+
+(add-hook! irony-mode
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async)
+
+  (irony-cdb-autosetup-compile-options))
+
+(add-hook! flycheck-mode 'flycheck-irony-setup)
+
+(set-email-account! "aramus92@gmail.com"
+                    '(
+                      ;; (user-mail-address . "aramus92@gmail.com") ;; only for mu < 1.4
+                      (user-full-name . "Nikolai Weidt")
+                      (smtpmail-smtp-server . "smtp.gmail.com")
+                      (smtpmail-smtp-user. "aramus92@gmail.com")
+                      (smtpmail-smtp-service  . 587)
+                      (smtpmail-stream-type   . starttls)
+                      (smtpmail-debug-info    . t)
+                      (mu4e-sent-folder . "/Gesendet")
+                      (mu4e-drafts-folder . "/Entwürfe")
+                      (mu4e-trash-folder . "/Papierkorb")
+                      (mu4e-refile-folder . "/Alle")
+                      (mu4e-compose-signature . "\nBest Regards\n\nNikolai Weidt")
+                      (mu4e-update-interval 300) ;; sec
+                      )
+                    t)
+
+(after! mu4e
+  (setq mu4e-maildir-shortcuts
+        '( (:maildir "/INBOX"              :key ?i)
+           (:maildir "/Gesendet"  :key ?s)
+           (:maildir "/Papierkorb"      :key ?t)
+           (:maildir "/Alle"   :key ?a)))
+  (when (fboundp 'imagemagick-register-types)
+  (imagemagick-register-types))
+  )
