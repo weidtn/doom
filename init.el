fc00e2e41feebcedfe3ca71a384ae01f54d9b307
weidtn <weidtn@gmail.com>
2020-11-05 12:05:49 +0100

    pretty flag for org

--- init.el
+++ init.el
@@ -145,6 +145,7 @@
         ;;+hugo            ; use Emacs for hugo blogging
         +jupyter        ; ipython/jupyter support for babel
         +pandoc          ; export-with-pandoc support
+        +pretty
         ;;+pomodoro        ; be fruitful with the tomato technique
         +present)        ; using org-mode for presentations
        ;;perl              ; write code no one else can comprehend
